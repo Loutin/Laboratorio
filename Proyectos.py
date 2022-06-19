6f81f438f1f7b8fcf3dde937e1d5077c61892fdc
Agustin <fatimaagustin33@hotmail.com>
2022-06-19 18:34:13 -0300

    Actualizando clase Clientes y clase Desarrolladores algo de main

--- Proyectos.py
+++ Proyectos.py
@@ -1,17 +1,9 @@
 class Proyectos:
     
-<<<<<<< HEAD
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
-=======
-    def __init__(self, id, name. cliente):
-        self.id = id
-        self.name = name
-        self.cliente = cliente
-    
->>>>>>> 1440fefa0ac3a465af8628578f27e3a071c006a8
     def clientes():
         """metodo clientes"""
    
@@ -21,14 +13,9 @@ class Proyectos:
     def lista_de_tareas(self, nro_tareas):
         nro_tareas = 0
 
-<<<<<<< HEAD
     #Lector de archivos
     Lista_prooyectos = open("C:\Users\fatim\Documents\Laboratorio_SO\Lab_SO\Listas\Lista_proyectos.txt")
     Lines = Lista_prooyectos.readlines()
-=======
-    
-
->>>>>>> 1440fefa0ac3a465af8628578f27e3a071c006a8
 
 
 
